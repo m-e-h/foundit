8a8e8ba8ca4eb410de58a76fa9fd131826811035
m-e-h <info@martyhelmick.com>
2015-11-25 12:27:54 -0500

    remove unused templates

--- components/entry-footer.php
+++
@@ -1,8 +0,0 @@
-<?php // if ( !is_archive() ) {
-    return
-//}
-?>
-<footer <?php hybrid_attr('entry-footer'); ?>>
-    <a href="<?php the_permalink(); ?>" class="mdl-button mdl-js-button mdl-js-ripple-effect"><?php esc_html_e( 'More', 'abraham' ); ?></a>
-    <?php get_template_part('components/child', 'links'); ?>
-</footer>
